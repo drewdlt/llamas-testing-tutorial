3450bc299c5c2a6e7637a2bcea81d4ef7208e009
Andrew De La Torre <drewdlt@yahoo.com>
2026-02-17 13:48:08 -0800

    completed code for areTheLlamasHappy function to ensure all test pass

--- src/main/java/HappyLlamas.java
+++ src/main/java/HappyLlamas.java
@@ -1,8 +1,21 @@
 
 public class HappyLlamas {
 
-    public boolean areTheLlamasHappy(boolean isNasaFabric, int numTrampolines) {
-        throw new UnsupportedOperationException("Not yet implemented");
+    public boolean areTheLlamasHappy(boolean ultraBouncy, int trampolines) {
+        // should return true if trampolines is between 24-42 INCLUSIVE!
+        // only changes if trampolines are made of ultra-bouncy Nasa fabric:
+        // if ultraBouncy is true, trampolines must be at LEAST 24, with no max value
+
+        // @param ultraBouncy True if trampolines are made of UltraBouncy Nasa fabric
+        // @param trampolines The number of trampolines
+        // @return boolean indicating if the llamas are happy
+
+        if (ultraBouncy) {
+            return trampolines >= 24;
+        }
+        else {
+            return trampolines >= 24 && trampolines <= 42;
+        }
     }
 
 }
