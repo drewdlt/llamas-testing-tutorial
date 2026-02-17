a3ccefeada5da0e0483df0011b42dffc26eeebe1
Andrew De La Torre <drewdlt@yahoo.com>
2026-02-17 11:56:16 -0800

    first commit

---
+++ src/main/java/example/llamas/LlamasApplication.java
@@ -0,0 +1,13 @@
+package example.llamas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlamasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LlamasApplication.class, args);
+    }
+
+}
